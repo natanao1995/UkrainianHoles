c0619631dad363b0e1a1888b3d560521d379ef53
Denys Yablonskyi <mrvayzard@ukr.net>
2019-08-10 02:12:43 +0300

    Ugly UI

---
+++ app/src/main/java/com/example/ukrainianholes/feature/add_hole/add/AddHoleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ukrainianholes.feature.add_hole.add
+
+import android.os.Bundle
+import com.example.ukrainianholes.R
+import com.example.ukrainianholes.architecture.base.BaseActivity
+
+class AddHoleActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_hole)
+    }
+}
