cd331a2631e965a846ab3521bf03e5fee4ba4266
Denys Yablonskyi <mrvayzard@ukr.net>
2019-08-10 12:51:42 +0300

    Add splash

--- app/src/main/java/com/example/ukrainianholes/architecture/base/BaseActivity.kt
+++ app/src/main/java/com/example/ukrainianholes/architecture/base/BaseActivity.kt
@@ -1,12 +1,20 @@
 package com.example.ukrainianholes.architecture.base
 
 import android.app.Activity
+import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ukrainianholes.R
 import com.pd.chocobar.ChocoBar
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+    }
+
     protected fun hideKeyboard() {
         val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         val view = currentFocus ?: View(this)
